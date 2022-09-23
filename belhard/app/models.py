120d0dfee60512a8f336bf9fd66fda4184def970
Shubovskaya <chibalice@yandex.ru>
2022-09-23 21:45:20 +0300

    add commit

--- belhard/app/models.py
+++ belhard/app/models.py
@@ -20,7 +20,10 @@ class Category(models.Model):
         verbose_name='описание',
         help_text='Макс. 24 символа'
     )
-    is_published = models.BooleanField(default=False, verbose_name='публикация')
+    is_published = models.BooleanField(
+        default=False,
+        verbose_name='публикация'
+    )
 
     def __str__(self):
         return self.name
@@ -30,3 +33,50 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('is_published', 'name')
+
+
+class Product(models.Model):
+    title = models.CharField(
+        max_length=36,
+        verbose_name='название',
+        help_text='Макс. 36 символов'
+    )
+    descr = models.CharField(
+        max_length=140,
+        null=True,
+        blank=True,
+        verbose_name='Артикль',
+        help_text='Макс. 16 символов'
+    )
+    article = models.CharField(
+        max_length=16,
+        unique=True,
+        verbose_name='Артикул',
+        help_text='Макс. 16 символов'
+    )
+    is_published = models.BooleanField(
+        default=False,
+        verbose_name='публикация'
+    )
+    price = models.DecimalField(
+        decimal_places=2,
+        max_digits=8,
+        default=0,
+        verbose_name='цена',
+        help_text='Макс. 999999.99'
+    )
+    count = models.PositiveSmallIntegerField(
+        default=0,
+        verbose_name='количество'
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.PROTECT,
+        verbose_name='категория'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'app_products'
